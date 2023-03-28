4547eb81e050e54355ee0e976d1205e7a8cc9ba2
Ulises Condori <129076093+UlisesCondori@users.noreply.github.com>
2023-03-27 22:33:12 -0300

    Update respuestas.sql

--- parte-1/homework/respuestas.sql
+++ parte-1/homework/respuestas.sql
@@ -1,7 +1,15 @@
---1
-select * from stg.product_master
+-- 1 Mostrar todos los productos dentro de la categoria electro junto con todos los detalles.
+select * 
+from stg.product_master
 where categoria = 'Electro'
 
---2
-select * from stg.product_master
+-- 2 ¿Cuáles son los productos producidos en China?
+select * 
+from stg.product_master 
 where origen = 'China'
+
+-- 3 Mostrar todos los productos de Electro ordenados por nombre.
+select * 
+from stg.product_master
+where categoria = 'Electro'
+ORDER BY NOMBRE 

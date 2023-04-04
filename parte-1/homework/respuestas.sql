2c61819ff51f10b59b3aa7dc2a6743e76c683de3
Ulises Condori <129076093+UlisesCondori@users.noreply.github.com>
2023-04-04 12:27:02 -0300

    Update respuestas.sql

--- parte-1/homework/respuestas.sql
+++ parte-1/homework/respuestas.sql
@@ -12,4 +12,29 @@ where origen = 'China'
 select * 
 from stg.product_master
 where categoria = 'Electro'
-ORDER BY NOMBRE 
+ORDER BY NOMBRE
+
+--4 Cuales son las TV que se encuentran activas para la venta?
+SELECT * 
+FROM STG.PRODUCT_MASTER 
+WHERE SUBCATEGORIA = 'TV' 
+AND IS_ACTIVE = 'true';
+
+--5 Mostrar todas las tiendas de Argentina ordenadas por fecha de apertura de las mas antigua a la mas nueva.
+SELECT * 
+FROM STG.STORE_MASTER 
+WHERE PAIS = 'Argentina' 
+ORDER BY FECHA_APERTURA ASC NULLS FIRST;
+
+--6 Cuales fueron las ultimas 5 ordenes de ventas?
+SELECT * 
+FROM STG.ORDER_LINE_SALE 
+ORDER BY FECHA DESC 
+LIMIT 5;
+
+--7 Mostrar los primeros 10 registros del conteo de tráfico por Super store ordenados por fecha.
+SELECT * 
+FROM STG.SUPER_STORE_COUNT 
+ORDER BY  cast(FECHA as date) ASC 
+LIMIT 10;
+
